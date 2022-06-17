2365d2198b8758734d631a274cf227547adc74c7
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-17 16:59:50 +0200

    Update reame file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Message.create(text: "Hello, world! 1")
-Message.create(text: "Hello, world! 2")
-Message.create(text: "Hello, world! 3")
-Message.create(text: "Hello, world! 4")
-Message.create(text: "Hello, world! 5")
+Message.create(text: "Hello, world!")
+Message.create(text: "Hello, Rails!")
+Message.create(text: "Hello, Kali!")
+Message.create(text: "Hello, React!")
+Message.create(text: "Hello, Mwape!")
