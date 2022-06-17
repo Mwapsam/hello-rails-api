623627ec6b7142b02a549c7820b71a9a896579d8
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-17 14:15:07 +0200

    Update routes

--- app/controllers/api/v1/hello_controller.rb
+++ app/controllers/api/v1/hello_controller.rb
@@ -1,4 +1,4 @@
-class V1::HelloController < ApplicationController
+class Api::V1::HelloController < ApplicationController
   def index
     @message = Message.find(Random.rand(1..5))
     render json: { texts: @message.text }.to_json
